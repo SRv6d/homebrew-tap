bb874e2874582650470d10584dac5132b74c53f9
Marvin Vogt <m@rvinvogt.com>
2024-12-18 22:56:15 +0100

    Use more conscice cask definition

--- Casks/hanko.rb
+++ Casks/hanko.rb
@@ -1,19 +1,14 @@
 cask "hanko" do
-    version "0.5.2"
+  arch arm: "aarch64", intel: "x86_64"
 
-    name "hanko"
-    desc "Keeps your allowed signers file up to date"
-    homepage "https://github.com/SRv6d/hanko"
-  
-    on_intel do
-      url "https://github.com/SRv6d/hanko/releases/download/v#{version}/hanko-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "3d4831c1b3b09c372bce1a7ae8d0f9136f719ffe9540df89e8b262c53b39dbd1"
-    end
-  
-    on_arm do
-      url "https://github.com/SRv6d/hanko/releases/download/v#{version}/hanko-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "0e428c457ccbac4ecdf42256fb8073bb238ab84eaac34b3a2cbe8f440a5537c2"
-    end
-  
-    binary "hanko"
-  end
+  version "0.5.2"
+  sha256 arm:   "0e428c457ccbac4ecdf42256fb8073bb238ab84eaac34b3a2cbe8f440a5537c2",
+         intel: "3d4831c1b3b09c372bce1a7ae8d0f9136f719ffe9540df89e8b262c53b39dbd1"
+
+  url "https://github.com/SRv6d/hanko/releases/download/v#{version}/hanko-v#{version}-#{arch}-apple-darwin.tar.gz"
+  name "hanko"
+  desc "Keeps your allowed signers file up to date"
+  homepage "https://github.com/SRv6d/hanko"
+
+  binary "hanko"
+end
