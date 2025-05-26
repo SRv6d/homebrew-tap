93464a8eb801d1a668a03590eb3c5ed3e9b3425a
Marvin Vogt <m@rvinvogt.com>
2025-05-26 19:13:19 +0200

    Bump cask hanko to v0.5.4

--- Casks/hanko.rb
+++ Casks/hanko.rb
@@ -1,9 +1,9 @@
 cask "hanko" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.5.3"
-  sha256 arm:   "91958ab11f4c9200ac1655b294fb613fa83ca6911a47dc66a439ee54a80d8cec",
-         intel: "481a5121c2a3514aedcbf4d6d0c93179503c0f660c43c5350232e81b534d266b"
+  version "0.5.4"
+  sha256 arm:   "dc9be0434761e64285ae75b9e2159b4da5a20fee5b01a9a0c661693f12113832",
+         intel: "93a9baf2469cde7b9e1cfa23b1ad077e316f5e13a3365efa3a270545b3af7d33"
 
   url "https://github.com/SRv6d/hanko/releases/download/v#{version}/hanko-v#{version}-#{arch}-apple-darwin.tar.gz"
   name "hanko"
