e7f653da63f446d40a184ec75fca6cdcbb782372
Marvin Vogt <m@rvinvogt.com>
2026-03-15 22:05:05 +0000

    Fix URL in hanko cask

--- Casks/hanko.rb
+++ Casks/hanko.rb
@@ -5,7 +5,7 @@ cask "hanko" do
   sha256 arm:   "ea918e2efd37f329f18ee7b7dc097ef6fe9dc9a5f0a5437e2a4900501160bcc1",
          intel: "438218a5ad1739f96b27551c8c97fcfb912aa63799d7baea52bf18bd8ed7e361"
 
-  url "https://github.com/SRv6d/hanko/releases/download/v#{version}/hanko-v#{version}-#{arch}-apple-darwin.tar.gz"
+  url "https://github.com/SRv6d/hanko/releases/download/v#{version}/hanko-#{version}-#{arch}-apple-darwin.tar.gz"
   name "hanko"
   desc "Keeps your allowed signers file up to date"
   homepage "https://github.com/SRv6d/hanko"
